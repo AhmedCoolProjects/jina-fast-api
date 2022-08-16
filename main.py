79cd5207e1f3053bca84cc40bc137ecedb7690f4
Ahmed Bargady <bargadyahmed2000@gmail.com>
2022-08-16 12:35:58 +0100

    adding surface page UI

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from functions.surface import surfaceFct
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 # run 'make dev' to test the application
 
@@ -19,7 +26,17 @@ class SurfaceParams(BaseModel):
     list_des_y: list
 
 
-@app.post("/fct/surface")
+@app.post("/fct/surface/api")
 def surface(params: SurfaceParams):
     s = surfaceFct(params.list_des_x, params.list_des_y)
     return {"La surface": s}
+
+
+@app.get("/fct/surface/api")
+def surface():
+    return {"message": "Welcome to Surface Function API"}
+
+
+@app.get("/fct/surface", response_class=HTMLResponse)
+async def surface_page(request: Request):
+    return templates.TemplateResponse("surface.html", {"request": request})
